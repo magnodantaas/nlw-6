6eaa2d7aaefdd45466ee9adb24776436e509db14
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-06-22 00:51:39 -0300

    Criating the migrations for database and controller, service, repository for user create

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,12 @@
 import express from "express";
+import "reflect-metadata";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Buscar informações
- * POST => Inserir (criar) uma informação
- * PUT => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH => Alterar uma informação específica
- */
+app.use(express.json());
 
-app.get("/test", (req, res) => {
-  // Request => Entrando
-  // Response => Saindo
-
-  return res.send("Olá")
-});
-
-app.post("/test-post", (req, res) => {
-  return res.send("Olá método post")
-});
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running"));
-
