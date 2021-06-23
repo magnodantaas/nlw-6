fa85d9662c4080035bbc6d7bb534ff20ef8c4b15
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-06-23 01:00:04 -0300

    Criating the migrations for database and controller, service, repository for tag create

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
-import express from "express";
 import "reflect-metadata";
-import "./database";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+
+  return res.status(500).json({
+    status: "Error",
+    error: "Internal Server Error"
+  });
+});
 
 app.listen(3000, () => console.log("Server is running"));
